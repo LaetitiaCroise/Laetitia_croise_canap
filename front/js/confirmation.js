da9cdb6a12ff5c2c6d9b0f9d4593d1ba15efecae
Croisé Laëtitia <110659766+LaetitiaCroise@users.noreply.github.com>
2022-11-17 09:49:08 +0100

    ok

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -4,4 +4,4 @@ console.log(url);
 url = new URL(url);// recupere tous les parametre d'url 
 console.log(url);
 orderId = document.getElementById('orderId');
-orderId.innerHTML = url.searchParams.get("id");// recherches le parametre id dans l'url
+orderId.innerHTML = url.searchParams.get("id");// renvoie le parametre id dans l'url
